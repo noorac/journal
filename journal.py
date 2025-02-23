239fcad767e74c5cc62915f13113fd0333c50342
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-02-23 19:02:11 +0100

    added ncurses

--- journal.py
+++ journal.py
@@ -10,9 +10,11 @@ Date: 2025-02-17
 # =========================
 # Imports
 # =========================
+import time
+import curses
+import logging
 import os
 import sys
-import logging
 
 # =========================
 # Constants
@@ -26,38 +28,85 @@ LOG_FILE = "script.log"
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler(LOG_FILE),
-        logging.StreamHandler(sys.stdout)
-    ]
+    handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler(sys.stdout)],
 )
 logger = logging.getLogger(__name__)
+
 
 # =========================
 # Helper Functions
 # =========================
-def clear_screen() -> None:
-    """
-    Clears the terminal
-    """
-    os.system("cls" if os.name == "nt" else "clear")
+
+def title_message(h,w,string):
+    sc.clear()
+    sc.addstr(h-h+1, w//2-len(string)//2,string)
+    sc.refresh()
+    time.sleep(0.75)
     return None
+
+def startup_curses():
+    curses.use_default_colors()
+    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_RED)
+    return None
+
+def draw_title():
+    height,width = sc.getmaxyx()
+    title = "Journal"
+    sc.addstr(height-height+1,width//2-len(title)//2,title,curses.color_pair(1))
+    return None
+
+def draw_menu():
+    height, width = sc.getmaxyx()
+    opt1 = "E) Press enter for new entry"
+    opt2 = "l) Press l for list"
+    opt3 = "q) press q to quit"
+    sc.addstr(height-height+3, width-width+1, opt1)
+    sc.addstr(height-height+4, width-width+1, opt2)
+    sc.addstr(height-height+5, width-width+1, opt3)
+    return None
+
 
 def get_filenames(directory):
     """Returns a list of filenames in the given directory."""
-    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    return [
+        f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))
+    ]
 
-def load_journals() :
+
+def load_journals():
     return 0
 
+def create_entry():
+    sc.clear()
+    draw_title()
+    sc.refresh()
+    key = -1
+    curses.echo()
+    sc.move(2,1)
+    entry = []
+    #Here is a bunch of mess because of different terminals call enter and 
+    #backspace different things. So need to cover bases.
+    while not ( key == curses.KEY_ENTER or key == 10 or key == 13 or key == "\n"):
+        key = sc.getch()
+        if key in ["ć", curses.KEY_BACKSPACE]:
+            sc.delch()
+            entry.pop(-1)
+        else:
+            entry.append(chr(key))
+    curses.noecho()
+    return entry
 
-class journal_entry :
+class journal_entry:
     def __init__(self, filename) -> None:
         self.filename = filename
-        self.filepath = "/home/noorac/.journal/"+self.filename
+        self.filepath = "/home/noorac/.journal/" + self.filename
         return None
+
     def new_entry(self):
-        self.entry = input("Whats your entry: \n")
+        self.entry_list = create_entry()
+        self.entry = ""
+        for i in range(len(self.entry_list)):
+            self.entry += self.entry_list[i]
         self.entry = self.entry + "\n\n"
         self.f = open(self.filepath, "a")
         self.f.write(self.entry)
@@ -67,12 +116,13 @@ class journal_entry :
     def load_entry(self):
         self.f = open(self.filepath, "r")
         for x in self.f:
-            print(x)
-        #print(self.f.read())
+            sc.addstr(x)
         self.f.close()
 
-def new_entry(journal_dict) :
+
+def new_entry(journal_dict):
     import datetime
+
     now = datetime.datetime.now()
     today = now.strftime("%Y-%m-%d")
     if today in journal_dict:
@@ -82,54 +132,72 @@ def new_entry(journal_dict) :
         journal_dict[today].new_entry()
     return 0
 
-def list_entries(journal_dict) :
-    journal_dict = journal_dict
-    clear_screen()
+
+def list_entries(journal_dict):
+    sc.clear()
+    h, w = sc.getmaxyx()
+    draw_title()
+    wherearewe = 0
     for i in range(len(journal_dict)):
+        sc.addstr(h-h+3+(i*2),w-w+1,f"{i}) {list(journal_dict.keys())[i]}")
         print(f"{i}) {list(journal_dict.keys())[i]}\n")
-    choise = input("Which you wanna cat?")
-    print(list(journal_dict.values())[int(choise)].load_entry())
-    wait = input("\nWaiting...")
-    
+        wherearewe = 3 + i*2 + 2
+    ans = sc.getkey(wherearewe,1)
+    sc.clear()
+    sc.addstr(str(list(journal_dict.values())[int(ans)].load_entry()))
+    sc.refresh()
+    sc.getch()
+    #wait = input("\nWaiting...")
+
     return 0
 
-def turn_dict (journal_list) -> dict:
+
+def turn_dict(journal_list) -> dict:
     journal_dict = {}
     if len(journal_list) > 0:
         for i in range(len(journal_list)):
             journal_dict[journal_list[i]] = journal_entry(journal_list[i])
     return journal_dict
 
-def menu (journal_dict) -> bool :
-    clear_screen()
+
+def menu(journal_dict) -> bool:
+    #clear_screen()
     journal_dict = journal_dict
     cont = True
-    print(f"Journal\n\n")
-    print(f"1) Press enter for new entry\n")
-    print(f"2) Press l for list of current entries\n\n")
-    print(f"q) Press q to quit\n\n")
-    ans = input(f"Option: ")
+    height,width = sc.getmaxyx()
+    sc.clear()
+    draw_title()
+    draw_menu()
+    sc.refresh()
+    ans = sc.getkey(8,1)
+    sc.refresh()
     try:
-        if ans not in ["","l","q"]:
-            print(f"Not an option")
-        if ans == "":
+        if str(ans) not in [curses.KEY_ENTER ,10 , 13, "\n","", "l", "q"]:
+            title_message(height,width,"Not an option..")
+        if ans == curses.KEY_ENTER or ans == 10 or ans == 13 or ans == "\n":
             new_entry(journal_dict)
-        if ans == "l":
+        if str(ans) == "l":
             list_entries(journal_dict)
-        if ans == "q":
+        if str(ans) == "q":
+            title_message(height,width,"Exiting ..")
             cont = False
     except ValueError:
         print(f"Not a string")
     return cont
 
+
 # =========================
 # Main Function
 # =========================
-def main():
+def main(stdscr):
     """
     Main function to run the script.
     """
     logger.info("Starting journal.py ..")
+    time.sleep(0.5)
+    global sc
+    sc = stdscr
+    startup_curses()
     filepath = "/home/noorac/.journal"
     journal_list = get_filenames(filepath)
     journal_dict = turn_dict(journal_list)
@@ -137,12 +205,13 @@ def main():
     while cont:
         cont = menu(journal_dict)
 
+
 # =========================
 # Entry Point
 # =========================
 if __name__ == "__main__":
     try:
-        main()
+        curses.wrapper(main)
     except KeyboardInterrupt:
         logger.warning("Script interrupted by the user.")
         sys.exit(1)
