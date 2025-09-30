13cd4d8cc9c801237263e86a3d530f63a094b70e
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-09-30 18:41:47 +0200

    fixed filename and docstring for filepath

--- journalentry.py
+++ journalentry.py
@@ -1,12 +1,14 @@
+
 from dataclasses import dataclass
 
 @dataclass
 class JournalEntry:
     """Creating each journal entry as an object"""
-    filename : str = filename
+    filename: str
     homefolder: str = "/home/noorac/.journal/"
 
     @property
     def filepath(self) -> str:
-        return homefolder + filename
+        """Creating the filepath dependant on the config file"""
+        return self.homefolder + self.filename
 
