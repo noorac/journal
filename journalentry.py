4a73eb7d9f3987e5d4f76856fbf8c578f4558e89
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-10-01 17:29:40 +0200

    updated _new_entry, though not finished

--- journalentry.py
+++ journalentry.py
@@ -4,11 +4,29 @@ from dataclasses import dataclass
 @dataclass
 class JournalEntry:
     """Creating each journal entry as an object"""
-    filename: str
-    homefolder: str = "/home/noorac/.journal/"
+    _filename: str
+    _homefolder: str = "/home/noorac/.journal/"
 
     @property
-    def filepath(self) -> str:
+    def _filepath(self) -> str:
         """Creating the filepath dependant on the config file"""
-        return self.homefolder + self.filename
+        return self._homefolder + self._filename
+
+    @property
+    def _new_entry(self, _entry_list) -> str:
+        """Creates a new entry. This will be appended to the self.filename"""
+        #_entry_list needs to be fixed, this is the list that comes when you
+        #type out a new entry. 
+        self._entry_list = []
+        # This is a string, and what we will actually append
+        self._new_entry = ""
+        for idx in range(len(self._entry_list)):
+            self._new_entry += self._entry_list[idx]
+        self._new_entry += "\n\n"
+        return self._new_entry
+#open the file
+# self._f = open(self._filepath, "a")
+# self._f.write(self._entry)
+# self._f.close()
+# return None
 
