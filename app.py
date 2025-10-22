d0b9b7fb9ece6f6d382fd9d9b8215240cbb44eda
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-10-22 18:47:18 +0200

    renamed journalservice and started on app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 #app: wires services + ui (no curses logic)
 
+
+from services import journalservice
+
+class App:
+    """
+    A starting point
+    """
+    def __init__(self) -> None:
+        self.journalservice = journalservice.JournalService
+
