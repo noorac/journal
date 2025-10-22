8496b9ba10074f968b2fc6aa2cc1cbc4701bc33a
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-10-22 18:44:38 +0200

    updates to uicontroller and journalentry, moved some code from entry to controller to store there

--- models/journalentry.py
+++ models/journalentry.py
@@ -27,33 +27,3 @@ class JournalEntry:
         """
         return self._filepath
 
-    def create_new_entry(self, sc) -> list[str]:
-        """Here we will request the inputs from the user"""
-        entry_list = []
-        key = -1
-        #Here is a bunch of mess because of different terminals call enter and 
-        #backspace different things. So need to cover bases. Essentially if 
-        #enter is pressed, the loop is ended and entry saved, if backspace is 
-        #pressed, it backspaces.
-        while not ( key == curses.KEY_ENTER or key == 10 or key == 13 or key == "\n"):
-            key = sc.getch()
-            if key in ["ć", curses.KEY_BACKSPACE]:
-                sc.delch()
-                entry_list.pop(-1)
-            else:
-                entry_list.append(chr(key))
-        return entry_list
-
-    def build_new_entry(self, sc) -> str:
-        """The data we get from the user is a list, we turn it into a string
-        to be saved here"""
-        return "".join(self.create_new_entry(sc)) + "\n\n"
-
-    def write_entry(self, sc) -> None:
-        """Write the entry to file"""
-        #open the file
-        self._f = open(self._filepath, "a")
-        self._f.write(self.build_new_entry(sc))
-        self._f.close()
-        return None
-
--- ui/uicontroller.py
+++ ui/uicontroller.py
@@ -1,7 +1,7 @@
 #uicontroller: event loop, screens, focus, routes keys
 import curses
 from menu.menu import Menu
-from renderer.renderer import Renderer
+from curses.renderer import Renderer
 
 class UIController:
     def __init__(self, stdscr, app):
@@ -10,10 +10,34 @@ class UIController:
         self.r = Renderer(stdscr)
         self.m = Menu()
 
-    #Methods
+    def run(self) -> None:
+        """
+        Main starting point for the whole thing
+        """
+        self.draw_menu()
+
+        pass
+
     def draw_menu(self) -> None:
         self.r.menu_lines(self.m.main_menu)
 
+
+    def create_new_entry(self, sc) -> list[str]:
+        """Here we will request the inputs from the user"""
+        entry_list = []
+        key = -1
+        #Here is a bunch of mess because of different terminals call enter and 
+        #backspace different things. So need to cover bases. Essentially if 
+        #enter is pressed, the loop is ended and entry saved, if backspace is 
+        #pressed, it backspaces.
+        while not ( key == curses.KEY_ENTER or key == 10 or key == 13 or key == "\n"):
+            key = sc.getch()
+            if key in ["ć", curses.KEY_BACKSPACE]:
+                sc.delch()
+                entry_list.pop(-1)
+            else:
+                entry_list.append(chr(key))
+        return entry_list
 
 
 
