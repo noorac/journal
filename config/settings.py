a5ade1b17701f7757da0cb5a4f662127802e825e
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-12-20 17:05:33 +0100

    started initial variables in __init__ in Settings, and created property to check if config-file exist

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,22 @@
 #settings: constants, keybinds, theme, config files etc
 
+import pathlib
+
 class Settings:
 
     def __init__(self) -> None:
         #create path to config file
         #initially hardcode this to ~/.config/journal/journal.conf
+        self._configdirectory = pathlib.Path("~/.config/journal").expanduser()
+        self._configdirectory.mkdir(parents=True, exist_ok=True)
+        self._configfile = self._configdirectory / "journal.conf"
+        self._configfile_exist = self._check_if_config_file_exist
         pass
-        
+
+    @property
+    def _check_if_config_file_exist(self) -> bool:
+        """
+        Checks if the config file already exist 
+        """
+        return self._configfile.is_file()
+
