558c4a42ed4d9d5d67f306b6b29d43a8bb225618
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-12-20 17:09:26 +0100

    finished creating paths for directory and files, and checking if file exists

--- config/settings.py
+++ config/settings.py
@@ -5,18 +5,23 @@ import pathlib
 class Settings:
 
     def __init__(self) -> None:
-        #create path to config file
-        #initially hardcode this to ~/.config/journal/journal.conf
+        #For now the config path is hardcoded to ~/.config/journal/journal.conf
+        
+        #Create directory- and filepath for the config file
         self._configdirectory = pathlib.Path("~/.config/journal").expanduser()
         self._configdirectory.mkdir(parents=True, exist_ok=True)
         self._configfile = self._configdirectory / "journal.conf"
+
+        #check if file exists
         self._configfile_exist = self._check_if_config_file_exist
+
+        
         pass
 
     @property
     def _check_if_config_file_exist(self) -> bool:
         """
-        Checks if the config file already exist 
+        Checks if the config file already exist by running is_file from pathlib
         """
         return self._configfile.is_file()
 
