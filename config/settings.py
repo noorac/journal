4318e1cde24bcdd056139925c96170ccb33058b5
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2025-12-20 16:49:03 +0100

    created class Settings with init in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,9 @@
 #settings: constants, keybinds, theme, config files etc
+
+class Settings:
+
+    def __init__(self) -> None:
+        #create path to config file
+        #initially hardcode this to ~/.config/journal/journal.conf
+        pass
+        
